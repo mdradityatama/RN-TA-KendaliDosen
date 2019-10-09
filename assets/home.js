9bb571a79eb7c01610934b92c562e8341c231498
mdradityatama <mdimasradityatama@gmail.com>
2019-10-09 13:41:03 +0700

    init

---
+++ assets/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
+        <Text>Ini Di HP Radit Gais</Text>
+        <Text> Makasih SayangQ RARA :) :*</Text>
+      </View>
+    );
+  }
+}
